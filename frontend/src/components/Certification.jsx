9220dcb65df94a3a0f217b9e495acefd45518397
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-05-30 14:16:13 +0530

    certification section completed

--- frontend/src/components/Certification.jsx
+++ frontend/src/components/Certification.jsx
@@ -24,8 +24,60 @@ const Certification = () => {
     const duplicatedCertifications = [...certifications, ...certifications, ...certifications];
 
   return (
-    <div className={}>
-      Cer
+    <div className={certificationStyles.container}>
+      <div className={certificationStyles.backgroundGrid}>
+        <div className={certificationStyles.topLine}></div>
+        <div className={certificationStyles.gridContainer}>
+            <div className={certificationStyles.grid}>
+                {Array.from({ length: 144}).map((_,i) => (
+                    <div key={i} className={certificationStyles.gridCell}></div>
+                ))}
+            </div>
+        </div>
+      </div>
+
+      <div className={certificationStyles.contentWrapper}>
+        <div className={certificationStyles.headingContainer}>
+            <div className={certificationStyles.headingInner}>
+                <div className={certificationStyles.leftLine}></div>
+                <div className={certificationStyles.rightLine}></div>
+
+                <h2 className={certificationStyles.title}>
+                    <span className={certificationStyles.titleText}>
+                        CERTIFIED & EXCELLENCE
+                    </span>
+                </h2>
+            </div>
+
+            <p className={certificationStyles.subtitle}>
+                Government recognized and internationally accredited healthcare stand
+            </p>
+            <div className={certificationStyles.badgeContainer}>
+                <div className={certificationStyles.badgeDot}></div>
+                <span className={certificationStyles.badgeText}>
+                    OFFICIALLY CERTIFIED
+                </span>
+            </div>
+        </div>
+
+        <div className={certificationStyles.logosContainer}>
+            <div className={certificationStyles.logosInner}>
+                <div className={certificationStyles.logosFlexContainer}>
+                    <div className={certificationStyles.logosMarquee}>
+                        {duplicatedCertifications.map((cert, index) => (
+                            <div key={`cert-${cert.id}-${index}`} className={certificationStyles.logoItem}>
+                                <div className="relative">
+                                    <img src={cert.image} alt={cert.name} className={certificationStyles.logoImage} />
+                                </div>
+                                <span className={certificationStyles.logoText}>{cert.name}</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+      <style>{certificationStyles.animationStyles}</style>
     </div>
   )
 }
