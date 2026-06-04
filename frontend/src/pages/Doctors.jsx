cd4e33f6af20dfbac183e5d6056688d5d6de9413
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-06-04 14:50:21 +0530

    doctors page helper functions added

---
+++ frontend/src/pages/Doctors.jsx
@@ -0,0 +1,15 @@
+import DoctorPage from "../components/DoctorPage"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+const Doctors = () => {
+  return (
+    <div>
+      <Navbar />
+      <DoctorPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default Doctors
