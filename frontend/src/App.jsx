30bbe23f843a4566340d9dcdfad8aaede9855f26
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-06-12 12:29:01 +0530

    doctor admin login added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import DoctorDetail from './pages/DoctorDetail'
 import Service from './pages/Service'
 import ServiceDetailPage from './pages/ServiceDetailPage'
 import Contact from './pages/Contact'
+import Login from './pages/Login'
 
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
         <Route path='/services' element={<Service />} />
         <Route path='/services/:id' element={<ServiceDetailPage />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/doctor-admin/login' element={<Login />} />
        </Routes>
     </div>
   )
---
+++ frontend/src/doctor/DoctorNavbar.jsx
@@ -0,0 +1,11 @@
+
+
+const DoctorNavbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DoctorNavbar
